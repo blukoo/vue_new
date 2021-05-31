9a5777508f2e411da1c075f7488f76b7a219337f
blukoo <dnehdrkd1234567@gmail.com>
2021-05-31 15:54:20 +0900

    second

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,23 +1,47 @@
 <template>
   <div>
-    <input type="text" v-model="newTodoItem">
-    <button v-on:click="addTodo">add</button>
+    <ul>
+        <li v-for="(todoItem,index) in todoItems" v-bind:key="todoItem">
+            <i> </i>
+            {{todoItem.item}}
+           
+            <span class="removeBtn" v-on:click="removeTodo(todoItem,index)">
+                <i class="fas fa-trash-alt">휴지통</i>
+            </span>
+        </li>
+        <!--<li>hi</li>
+        <li>hello</li>
+        <li>bye</li>
+        <li>adfaff</li>-->
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-    data: function(){
-        return{
-            newTodoItem:""
-        }
+    data:function(){
+        return{todoItems:[]}
     },
     methods:{
-        addTodo:function(){
-            localStorge.setItem()
+        removeTodo :function(todoItem, index){
+            console.log(todoItem, index);
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index,1)
+        }
+    },
+    created:function(){
+        if(localStorage.length>0){
+            for(var i=0; i<localStorage.length; i++){
+                    if(localStorage.key(i)!=='loglevel:webpack-dev-server'){
+                
+               this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+                //console.log(localStorage.key(i))
+                    }
+                }
+            }
         }
     }
-}
+
 </script>
 
 <style>
