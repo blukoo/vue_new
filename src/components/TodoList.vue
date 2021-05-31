02a00e3aa5b63b27d8ae6a637dc437b5572b433d
blukoo <dnehdrkd1234567@gmail.com>
2021-05-31 11:02:21 +0900

    first

---
+++ src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input type="text" v-model="newTodoItem">
+    <button v-on:click="addTodo">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return{
+            newTodoItem:""
+        }
+    },
+    methods:{
+        addTodo:function(){
+            localStorge.setItem()
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
